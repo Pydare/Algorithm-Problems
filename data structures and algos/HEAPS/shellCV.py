f92c3e5686bb1773e706e58e40a4a1a040fad857
Pydare <dareyadewumi650@gmail.com>
2019-09-09 15:51:24 +0100

    on it

---
+++ data structures and algos/HEAPS/shellCV.py
@@ -0,0 +1,2 @@
+#- Maintenance support of equipment, valves and vessels of equipment on the Bonga FPSO
+#- Helped increased in the oil production during my stay, by ensuring equipment work at almost 100% efficiency
