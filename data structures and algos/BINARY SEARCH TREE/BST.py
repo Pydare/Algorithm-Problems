3d464b4b6ad565e55edcc290f5f53254ebe13493
Pydare <dareyadewumi650@gmail.com>
2019-10-29 13:29:57 +0100

    regular day to days

--- data structures and algos/BINARY SEARCH TREE/BST.py
+++ data structures and algos/BINARY SEARCH TREE/BST.py
@@ -1,15 +1,15 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self,value):
         self.info = value
         self.lchild = None
         self.rchild = None
 
-class BinarySearchTree:
+class BinarySearchTree: 
     def __init__(self):
         self.root = None
-    def isempty(self): 
+    def isempty(self,x):
         return self.root == None
-    def insert(self, x):
+    def insert(self,x):
         self.root = self._insert(self.root,x)
     def _insert(self,p,x):
         if p is None:
@@ -17,11 +17,12 @@ class BinarySearchTree:
         elif x < p.info:
             p.lchild = self._insert(p.lchild,x)
         elif x > p.info:
-            p.rchild = self._insert(p.rchild, x)
+            p.rchild = self._insert(p.rchild,x)
         else:
-            print(x, ' already present in  the tree')
+            print(x,' already present in the tree')
         return p
-    def insert1(self, x):
+
+    def insert1(self,x):
         p = self.root
         par = None
         while p is not None:
@@ -31,7 +32,7 @@ class BinarySearchTree:
             elif x > p.info:
                 p = p.rchild
             else:
-                print(x + ' already pressent in the tree')
+                print(x + ' already present in the tree')
                 return
         temp = Node(x)
         if par == None:
@@ -39,24 +40,26 @@ class BinarySearchTree:
         elif x < par.info:
             par.lchild = temp
         else:
-            par.rchild = temp
+            par.rchild = temp 
+
+
     def search(self,x):
-        return self._search(self.root, x) is not None
+        return self._search(self.root,x) is not None
     def _search(self,p,x):
         if p is None:
-            return None #key not found
+            return None
         if x < p.info:
-            return self._search(p.lchild, x) #search in left subtre
+            return self._search(p.lchild,x)
         if x > p.info:
-            return self._search(p.rchild, x) #search in right subtree
-        return p #key found
+            return self._search(p.rchild,x)
+        return p
     def search1(self,x):
         p = self.root
         while p is not None:
             if x < p.info:
-                p = p.lchild #move to left child
+                p = p.lchild
             elif x > p.info:
-                p = p.rchild #move to right child
+                p = p.rchild
             else:
                 return True
         return False
@@ -69,14 +72,14 @@ class BinarySearchTree:
         return p.info
     def max1(self):
         if self.isempty():
-            print('Tree is empty')
+            print('Tree is empty dawg')
         p = self.root
         while p.rchild is not None:
             p = p.rchild
         return p.info
     def min2(self):
-        if self.isempty():
-            print('Tree is empty')
+        if self.empty():
+            print('Tree is still empty')
         return self._min(self.root).info
     def _min(self,p):
         if p.lchild is None:
@@ -85,59 +88,61 @@ class BinarySearchTree:
     def max2(self):
         if self.isempty():
             print('Tree is empty')
-        return self._max(self.root).info
+        return self._max(self.root).info 
     def _max(self,p):
         if p.rchild is None:
             return p
         return self._max(p.rchild)
-
     def delete1(self,x):
         p = self.root
         par = None
+
         while p is not None:
             if x == p.info:
                 break
-            par = p 
+            par = p
             if x < p.info:
                 p = p.lchild
             else:
                 p = p.rchild
         if p == None:
-            print(x, ' not found')
+            print(x,' is not found')
             return
-        #case c: 2 children
-        #find inorder successor and its parent
-        if p.lchild is not None and p.rchild is not None:
-            ps = p
-            s = p.rchild
+        #case c: 2 children 
+        #find the inorder successor and it's parent
+        #the inoder successor is the left most child in the right subtree
 
-            while s.lchild is not None:
+        if p.lchild is not None and p.rchild is not None:
+            ps = p #the parent of p is now made p
+            s = p.rchild #the right child of p for the inorder sucessor process
+
+            while s.lchild is not None: #looking for left most child
                 ps = s
                 s = s.lchild
-            p.info = s.info
+            p.info = s.info #copying the successor into node to be deleted
             p = s
             par = ps
-        #case b and case a: 1 or no child
-        if p.lchild is not None: #node to be deleted has left child
-            ch = p.lchild
-        else:       #node to be deleted has right child or no child
-            ch = p.rchild
-        if par == None: #node to be deleted is root node
-            self.root = ch
-        elif p == par.lchild:  #node is left child of its parent
-              par.lchild = ch
-        else:
-            par.rchild = ch
-        
+            if p.lchild is not None:
+                ch = p.lchild
+            else p.rchild is not None:
+                ch = p.rchild
+            
+            if par == None:
+                self.root = ch
+            elif p == par.lchild:
+                par.lchild = ch
+            else:
+                par.rchild = ch
+
     def delete(self,x):
         self.root = self._delete(self.root,x)
     def _delete(self,p,x):
         if p is None:
             print(x,' not found')
             return p
-        if x < p.info: #delete from left subtree
+        if x < p.info:
             p.lchild = self._delete(p.lchild,x)
-        elif x > p.info: #delete from right subtree
+        elif x > p.info:
             p.rchild = self._delete(p.rchild,x)
         else:
             #key to be deleted is found
@@ -147,10 +152,13 @@ class BinarySearchTree:
                     s = s.lchild
                 p.info = s.info
                 p.rchild = self._delete(p.rchild,s.info)
-            else: #1 child or no child
+            else:
                 if p.lchild is not None:
                     ch = p.lchild
                 else:
                     ch = p.rchild
                 p = ch
-        return p 
+        return p
+
+
+                
