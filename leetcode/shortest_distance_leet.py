28f2e63223e1303376794ff4f39d117ae3249634
Dare Adewumi <42492589+Pydare@users.noreply.github.com>
2020-11-25 19:31:55 +0100

    Update shortest_distance_leet.py

--- leetcode/shortest_distance_leet.py
+++ leetcode/shortest_distance_leet.py
@@ -2,29 +2,31 @@ from collections import deque
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        min_distance = float('inf')            
+        min_distance = float('inf') 
+        total_ones = 0
+        
+        # count the total number of ones in the grid
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    total_ones += 1
         
         # loop through all empty lands and find the total diatance to other buildings
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    temp = self.building_distance(i, j, grid)
+                    temp = self.building_distance(i, j, grid, total_ones)
                     min_distance = min(min_distance, temp)
                     
                     
         return -1 if min_distance == 0 or min_distance == float('inf') else min_distance
     
-    def building_distance(self, i, j, grid):
+    def building_distance(self, i, j, grid, total_ones):
         seen = set((i,j))
         m, n = len(grid), len(grid[0])
         total = 0
         directions = [(0,1),(0,-1),(1,0),(-1,0)] # right, left, down, up
         q = deque([((i,j), 0)])
-        total_ones = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    total_ones += 1
         
         ones = 0
         while q:
